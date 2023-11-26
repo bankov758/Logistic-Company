c7ed989b13b672b5555c5bdcd861f421deb470fe
Ivo <ivobankov758@gmail.com>
2023-11-26 18:44:10 +0200

    Initial commit

---
+++ logistic-company/src/main/java/com/nbu/logisticcompany/LogisticCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.nbu.logisticcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticCompanyApplication.class, args);
+	}
+
+}
---
+++ logistic-company/src/test/java/com/nbu/logisticcompany/LogisticCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbu.logisticcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
