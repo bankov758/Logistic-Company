dcdd72fc6ae3bc99533fbac55ed3ece2b7d090b3
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-04 21:49:13 +0200

    clear the unnecessary markup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,10 @@
-import React, {useEffect} from 'react';
 import './App.css';
 
 function App() {
 
-    const fetchUsers = async () => {
-        const response = await fetch("http://localhost:8080/api/users", {
-            method: "GET",
-            headers: {
-                Authorization: 'antoan'
-            }
-        });
-
-        console.log('response >>> ', response);
-
-        const data = await response.json();
-
-        console.log('data >>> ', data);
-    }
-
-    useEffect(() => {
-        fetchUsers()
-    }, []);
-
     return (
         <div className="App">
-
+            <h1>app</h1>
         </div>
     );
 }
