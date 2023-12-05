fa8224348c14ca10a513e37e72ec32cd58e46903
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-05 21:24:29 +0200

    Index route

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -1,10 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 
 const ProductDetails: React.FC = () => {
+    const params = useParams();
 
+    useEffect(() => {
+      console.log('params >>> ', params)
+    })
+    
     return (
         <Fragment>
             <h1>Product details</h1>
+            <span>ProductID: {params.productId}</span>
+
+            {/* relative to the path, not to the route since it will go to the home page because .. is a relative path */}
+            <p><Link to='..' relative='path'>Go back</Link></p>
         </Fragment>
     )
 }
--- frontend/src/util/routes.tsx
+++ frontend/src/util/routes.tsx
@@ -7,6 +7,7 @@ import RootLayout from '../components/Root';
 import Home from '../pages/Home';
 import Auth from '../pages/Auth';
 import ProductPage from '../pages/Products';
+import ProductDetails from '../pages/ProductDetails';
 import ErrorPage from '../components/Error';
 
 
@@ -17,16 +18,22 @@ const router = createBrowserRouter([
         errorElement: <ErrorPage />,
         children: [
         //every object represents one route
+        //using a relative paths, not an absolute path
             {
-                path: '/',//pathname
-                element: <Home />//JSX element (component)
+                index: true,//turns this route to so called index route (default route) that should be displayed if the parent route's path is currently loaded
+                // path: '',//pathname
+                element: <Home />,//JSX element (component)
             },
             {
-                path: '/products',//pathname
-                element: <ProductPage />//JSX element (component)
+                path: 'products',//pathname
+                element: <ProductPage />,//JSX element (component)
             },
             {
-                path: '/auth',//pathname
+                path: 'products/:productId',//dynamic pathname segment
+                element: <ProductDetails />//JSX element (component)
+            },
+            {
+                path: 'auth',//pathname
                 element: <Auth />//JSX element (component)
             }
         ]
