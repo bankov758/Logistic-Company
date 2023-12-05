d50bc6038a46bef67342fcca9af99bd26e2d78ab
“antoanparashkevov” <tonkata1505@gmail.com>
2023-12-05 21:07:05 +0200

    Product details

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
 
     return (
         <Fragment>
             <h1>home page</h1>
-            <Link to='/auth'>Auth page</Link>
         </Fragment>
     )
 }
