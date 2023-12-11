9fa816052ecf0eceb87cb8003f745b4dddf4f1a2
Eli1396 <elizabet_di@abv.bg>
2023-12-11 18:15:57 +0200

    test

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -3,7 +3,26 @@ import React from "react";
 const Footer: React.FC = () => {
 
     return (
-        <h1>footer</h1>
+        <div className="footer">
+             <div className="sb_footer section_padding">
+                 <div className="sb_footer-links">
+                     <div className="sb_footer-links-div">
+                         <h4>More info:</h4>
+                    </div>
+                    <div className="sb_footer-links-div">
+                         <a href="/aboutus">
+                             <p>About us</p>
+                         </a>
+                    </div>
+                    <div className="sb_footer-links-div">
+                         <a href="/contactus">
+                             <p>Contact us</p>
+                         </a>
+                     </div>
+                  </div>
+             </div>
+        </div>
+
     )
 }
 
