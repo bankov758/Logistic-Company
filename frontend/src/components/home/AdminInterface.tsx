16a4447ee23f0de14b5577cd818902337f29d359
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-21 22:36:11 +0200

    Configure the clients table, employees table, offices table. Add a dropdown menu for selecting a company, add a dialog for showing a detailed information about the company

--- frontend/src/components/home/AdminInterface.tsx
+++ frontend/src/components/home/AdminInterface.tsx
@@ -1,51 +1,89 @@
 "use client";
-import { categories, data, officeColumnes, tableColumns } from "@/data/admin/ordersTableData";
+import React, {useState} from "react";
+
 import Table from "./EmployeeInterface/Table";
+import DataSelectorWrapper, {selectorItem} from "@/components/UI/DataSelectorWrapper";
+import BaseDialog from "@/components/UI/BaseDialog";
 
+import MessageIcon from '../../../public/icons/message.svg';
 
-import Dropdown from "./AdminInterface/Dropdown";
+import {
+    categories,
+    data,
+    officeCategories,
+    officeColumns,
+    officeDataItems,
+    tableColumns
+} from "@/data/admin/ordersTableData";
+import Image from "next/image";
 
-import React, { useState } from "react";
+let selectorData: selectorItem[] = [
+    {
+        title: 'Econt',
+        code: 'econt',
+    },
+    {
+        title: "Speedy",
+        code: "speedy"
+    },
+    {
+        title: "DHL",
+        code: "dhl"
+    }
+];
 
-const AdminInterface = () => { 
-    interface Option {
-        label: string;
-        value: string;
-      }
-    const options: Option[] = [
-        { label: "Company 1", value: "1" },
-        { label: "Company 2", value: "2" },
-        { label: "Company 3", value: "3" },
-      ];
-      const [selectedOption, setSelectedOption] = useState<Option | undefined>(undefined);
+const AdminInterface: React.FC = () => {
+    const [selectedCompany, setSelectedCompany] = useState(selectorData[0]);
+    const [showDialog, setShowDialog] = useState<boolean>(false)
 
-      const handleOptionSelect = (option: Option) => {
-        setSelectedOption(option);
-        // Perform any other actions needed when an option is selected
-      };
+    const updateSelectedCompany = (data: selectorItem) => {
+        console.log(data);
+        setSelectedCompany(data);
+    }
+
     return <>
-        <h3 className="flex justify-start w-full">Welcome user ...! This is your home page. You're logged in as admin.</h3>
+        <h3 className="flex justify-start w-full">
+            Welcome user ...! This is your home page. You&apos;re logged in as admin.
+        </h3>
+
         <br/>
 
-        <Dropdown
-         options={options}
-         selectedOption={selectedOption}
-          onSelect={handleOptionSelect}
-        />
-        
-        
-        <Table
-                columns={tableColumns}
-                categories={categories}
-                data={data}
+        <div className='flex justify-center items-center gap-x-10'>
+            <DataSelectorWrapper
+                hasInitialPlaceholderValue
+                placeholderValue={selectedCompany.title}
+                selectorData={selectorData}
+                onResubForNewData={updateSelectedCompany}
             />
+
+            <Image
+                src={MessageIcon}
+                alt='Message'
+                className='cursor-pointer'
+                onClick={() => setShowDialog(true)}
+            />
+        </div>
+        {showDialog &&
+            (
+                <BaseDialog title='test title' tryClose={() => setShowDialog(false)}>
+                    test
+                </BaseDialog>
+            )
+        }
+
+        {/*/!* clients + employees *!/*/}
         <Table
-            columns={officeColumnes}
+            columns={tableColumns}
             categories={categories}
             data={data}
         />
 
-
+        {/* offices */}
+        <Table
+            columns={officeColumns}
+            categories={officeCategories}
+            data={officeDataItems}
+        />
     </>;
 }
 
