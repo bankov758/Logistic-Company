02ae426ea79263d8f544b3e326b3409d51d01771
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-27 14:57:13 +0200

    Add register mechanism in the frontend

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,47 +1,75 @@
+"use client";
+import {Fragment} from "react";
+
 import Link from "next/link";
+import { useFormState } from "react-dom";
+
+import {register} from "@/lib/actions";
 
 const RegistrationPage = () => {
-    return <div className="bg-white p-40 rounded-xl shadow-md text-center">
-        <div className="mb-5">
-            <h2>Register</h2>
-        </div>
+    const [formState, registerAction] = useFormState(register, { message: null, errors: '' })
+    console.log(formState)
+    return (
+        <Fragment>
+            {formState.errors && typeof formState.errors === 'string' &&
+                <div>{formState.errors}</div>
+            }
+            {formState.errors && typeof formState.errors === 'object' &&
+                <div>
+                    {
+                        formState.errors
+                            .map((error, index)=>
+                                <p key={index}>{error.message}</p>
+                            )
+                    }
+                </div>
+            }
+            <div className="bg-white p-40 rounded-xl shadow-md text-center">
+                <div className="mb-5">
+                    <h2>Register</h2>
+                </div>
 
-        <form>
-            <div className="input-group">
-                <label className="block">Username</label>
-                <input type="text" name="userName"
-                       className="block w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                <form action={registerAction}>
+                    <div className="input-group">
+                        <label className="block">Username</label>
+                        <input type="text" name="username"
+                               className="block p-3 w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                    <div className="input-group">
+                        <label className="block">Firstname</label>
+                        <input type="text" name="firstName"
+                               className="block p-3 w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                    <div className="input-group">
+                        <label className="block">Lastname</label>
+                        <input type="text" name="lastName"
+                               className="block p-3 w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                    <div className="input-group">
+                        <label className="block">Password</label>
+                        <input type="password" name="password"
+                               className="block p-3 w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                    <div className="input-group">
+                        <label className="block">Password</label>
+                        <input type="password" name="confirmPassword"
+                               className="block p-3 w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
+                    </div>
+                    <div className="flex justify-center">
+                        <button
+                            type="submit"
+                            className="block px-3 py-1.5 mt-1  relative rounded-xl bg-green-400 text-white border-none cursor-pointer"
+                        >
+                            Register
+                        </button>
+                    </div>
+                    <p>
+                        Already a member? <Link href="/login"> Sign in </Link>
+                    </p>
+                </form>
             </div>
-            <div className="input-group">
-                <label className="block">Firstname</label>
-                <input type="text" name="firstName"
-                       className="block w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
-            <div className="input-group">
-                <label className="block">Lastname</label>
-                <input type="text" name="lastName"
-                       className="block w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
-            <div className="input-group">
-                <label className="block">Password</label>
-                <input type="password" name="password_1"
-                       className="block w-full rounded-xl border-0 py-1.5  ring-1 ring-inset ring-gray-400 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></input>
-            </div>
-            <div className="input-group">
-                <label className="block">Confirm password</label>
-                <input type="password" name="password_2" className="input-style"></input>
-            </div>
-            <div className="flex justify-center">
-                <button type="submit"
-                        className="block px-3 py-1.5 mt-1  relative rounded-xl bg-green-400 text-white border-none cursor-pointer"
-                        name="reg_user">Register
-                </button>
-            </div>
-            <p>
-                Already a member? <Link href="/login"> Sign in </Link>
-            </p>
-        </form>
-    </div>
+        </Fragment>
+    )
 }
 
 export default RegistrationPage;
--- frontend/src/lib/actions.ts
+++ frontend/src/lib/actions.ts
@@ -3,21 +3,21 @@
 import { z } from "zod";
 
 const loginSchema = z.object({
-	email: z.string().trim().email({ message: "Invalid email address!" }),
+	username: z.string().trim().email({ message: "Invalid email address!" }),
 	password: z
 		.string()
 		.trim()
 		.min(4, { message: "You've provided an invalid password!" })
 });
 
-export async function login(initialState: any, formData: FormData) {
+export const login = async (initialState: any, formData: FormData) => {
 	const fields = {
-		email: (formData.get("email") as string) || "",
+		username: (formData.get("username") as string) || "",
 		password: (formData.get("password") as string) || ""
 	};
 
 	const validateFields = loginSchema.safeParse({
-		email: fields.email,
+		username: fields.username,
 		password: fields.password
 	});
 
@@ -49,4 +49,86 @@ export async function login(initialState: any, formData: FormData) {
 		errors: null,
 		message: "You've successfully logged in!"
 	};
+}
+
+const registerSchema = z.object({
+	username: z.string().trim().min(4, { message: "You should provide a username with at least 4 characters!" }),
+	firstName: z.string(),
+	lastName: z.string(),
+	password: z
+		.string()
+		.trim()
+		.min(4, { message: "You've provided an invalid password!" }),
+	confirmPassword: z
+		.string()
+		.trim()
+});
+
+export const register = async (initialState: any, formData: FormData) => {
+	const username = formData.get('username') as string;
+	const firstName = formData.get('firstName') as string;
+	const lastName = formData.get('lastName') as string;
+	const password = formData.get('password')! as string;
+	const confirmPassword = formData.get('confirmPassword')! as string;
+
+	const fields = {
+		username,
+		firstName,
+		lastName,
+		password,
+		confirmPassword
+	}
+
+	if( password.trim() !== confirmPassword.trim() ) {
+		return {
+			message: '',
+			errors: "Passwords do not match!"
+		}
+	}
+
+	const validateSchema = registerSchema.safeParse(fields);
+
+
+	if (!validateSchema.success ) {
+		return {
+			message: "",
+			errors: validateSchema.error.issues
+		}
+	}
+
+	try {
+		//make an API call to the server to login the user
+		const response = await fetch('http://localhost:8080/api/users', {
+			method: "POST",
+			body: JSON.stringify(fields),
+			headers: {
+				'Content-Type': "application/json",
+				Accept: "*/*"
+			}
+		})
+
+		if( !response.ok ) {
+			throw new Error("Something happened! Registration process was unsuccessful!")
+		}
+
+		const data = await response.json();
+
+		return {
+			errors: '',
+			message: data
+		}
+
+	} catch ( error ) {
+		if( error instanceof Error ) {
+			return {
+				errors: error.message || "Something went wrong!",
+				message: ""
+			};
+		}
+
+		return {
+			errors: "Something went wrong!",
+			message: ""
+		};
+	}
 }
