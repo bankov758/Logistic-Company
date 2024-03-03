8cfd1380466db0060df3c4fbb73a0468873ea4e1
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-03 21:57:40 +0200

    Choose the appropriate interface depending on the active roles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,63 @@
-import React, { Fragment } from "react";
+"use client";
+import React, {Fragment, useEffect, useState} from "react";
+import {getSession} from "@/lib/auth";
 
 import ClientInterface from "@/components/home/ClientInterface";
 import EmployeeInterface from "@/components/home/EmployeeInterface";
 import AdminInterface from "@/components/home/AdminInterface";
+import BaseDialog from "@/components/UI/BaseDialog";
+
 
 const Home: React.FC = () => {
-  return (
-		<Fragment>
-			<ClientInterface/>
-			<EmployeeInterface/>
-			<AdminInterface/>
-		</Fragment>
-	);
+	const [session, setSession] = useState<null | {username: string; roles: string[]}>();
+    const [selectedInterface, setSelectedInterface] = useState<string | null>(null);
+    const [showDialog, setShowDialog] = useState<boolean>(true);
+
+    useEffect(() => {
+        getSession()
+            .then((response) => {
+                setSession(response);
+                if (response && response.roles.includes("USER") && !response.roles.includes("EMPLOYEE")) {
+                    setSelectedInterface("user");
+                    setShowDialog(false);
+                }
+            })
+    }, [session]);
+
+    const selectInterface = (selectedInterface: string) => {
+        setSelectedInterface(selectedInterface)
+        setShowDialog(false);
+    }
+
+    return (
+        <Fragment>
+            {	session && showDialog &&
+                (
+					session.roles.includes("EMPLOYEE") && !session.roles.includes("ADMIN") ?
+						<BaseDialog title="Choose an interface">
+							<button onClick={() => selectInterface('user')}>User</button>
+							<button onClick={() => selectInterface('employee')}>Employee</button>
+						</BaseDialog> :
+					session.roles.includes("ADMIN") ?
+                        <BaseDialog title="Choose an interface">
+                            <button onClick={() => selectInterface('user')}>User</button>
+                            <button onClick={() => selectInterface('employee')}>Employee</button>
+                            <button onClick={() => selectInterface("admin")}>Admin</button>
+                        </BaseDialog> :
+                        null
+                )
+            }
+            {
+                selectedInterface === "user" ?
+                    <ClientInterface /> :
+                selectedInterface === "employee" ?
+                    <EmployeeInterface /> :
+                selectedInterface === "admin" ?
+                    <AdminInterface /> :
+                    null
+            }
+        </Fragment>
+    );
 }
 
 export default Home;
