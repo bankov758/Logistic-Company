6a8f3ac8716c2e1eeedf568fad1d9b546c099df2
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-04 16:11:01 +0200

    Remove placeholders

--- frontend/src/components/home/AdminInterface/EditOfficeForm.tsx
+++ frontend/src/components/home/AdminInterface/EditOfficeForm.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import {item} from "@/components/home/Table";
 
-const EditOfficeForm: React.FC<{ selectedItem: item}> = ({ selectedItem }) => {
+const EditOfficeForm: React.FC<{ selectedItem: item }> = ({selectedItem}) => {
+
     return (
         <form>
             <h3 className="flex justify-center">Edit an office:</h3><br/>
             <div className="order-div">
                 <label className="block  text-gray-500">Address:</label>
-                <input type="text" id="adress" className="input-info-dialog"
-                       placeholder={selectedItem?.address ?? "Address:"}></input><br/>
+                <input type="text" id="adress" className="input-info-dialog"/>
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Company name:</label>
-                <input type="text" id="companyName" className="input-info-dialog"
-                       placeholder={selectedItem?.company_name ?? "Name:"}></input><br/>
+                <input type="text" id="companyName" className="input-info-dialog"/>
             </div>
             <div className='flex justify-center py-3 text-gray-500'>
                 <button
--- frontend/src/components/home/EmployeeInterface/EditShipmentForm.tsx
+++ frontend/src/components/home/EmployeeInterface/EditShipmentForm.tsx
@@ -3,6 +3,7 @@
 import React, {useState} from "react";
 import {item} from "@/components/home/Table";
 import RadioButton from "@/components/UI/RadioButton";
+import useInput from "@/hooks/useInput";
 
 // Example usage:
 const options = [
@@ -10,55 +11,48 @@ const options = [
     {value: "Closed", placeholder: "Closed"},
 ];
 
-const EditShipmentForm: React.FC<{ selectedItem: item}> = ({ selectedItem }) => {
-    const [inputType, setInputType] = useState("text");
+const EditShipmentForm: React.FC<{ selectedItem: item }> = ({selectedItem}) => {
 
     return (
         <form>
             <h3 className="flex justify-center">Edit the shipment</h3>
-            <br/>
             <div className="order-div">
                 <label className="block  text-gray-500">Sender:</label>
-                <input type="text" id="sender" className="input-info-dialog"
-                       placeholder={selectedItem?.sender ?? "Name:"} />
+                <input type="text" id="sender" className="input-info-dialog" />
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Receiver:</label>
-                <input type="text" id="receiver" className="input-info-dialog"
-                       placeholder={selectedItem?.receiver ?? "Name:"}></input><br/>
+                <input type="text" id="receiver" className="input-info-dialog" />
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Departure location:</label>
-                <input type="text" id="departureAddress" className="input-info-dialog"
-                       placeholder={selectedItem?.departureAddress ?? "Departure location:"}></input><br/>
+                <input type="text" id="departureAddress" className="input-info-dialog" />
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Arrival location:</label>
-                <input type="text" id="arrivalAddress" className="input-info-dialog"
-                       placeholder={selectedItem?.arrivalAddress ?? "Arrival location:"}></input><br/>
+                <input type="text" id="arrivalAddress" className="input-info-dialog" />
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Date:</label>
-                <input type={inputType} onFocus={() => setInputType('date')}
-                       onBlur={() => setInputType('text')} id="sentDate"
-                       className="block text-gray-500 rounded-xl border-2 py-1.5 px-1 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6 whitespace-pre-line"
-                       placeholder={selectedItem?.sentDate ?? "Date: "}></input><br/>
+                <input
+                    type='date'
+                    id="sentDate"
+                    className="block text-gray-500 rounded-xl border-2 py-1.5 px-1 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-base sm:leading-6 whitespace-pre-line"
+                />
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Status:</label>
                 <div className="rounded-xl border-2 py-1 px-3">
-                    <RadioButton options={options}/>
+                    <RadioButton options={options} />
                 </div>
             </div>
             <div className="order-div">
                 <label className="block  text-gray-500">Weight (in kilodram):</label>
-                <input type="text" id="weight" className="input-info-dialog"
-                       placeholder={selectedItem?.weight ?? "Weight:"}></input><br/>
+                <input type="text" id="weight" className="input-info-dialog" />
             </div>
             <div className="order-div ">
                 <label className="block  text-gray-500">Employee:</label>
-                <input type="text" id="employee" className="input-info-dialog"
-                       placeholder={selectedItem?.employee ?? "Employee:"}></input><br/>
+                <input type="text" id="employee" className="input-info-dialog" />
             </div>
             <div className='flex justify-center py-3 text-gray-500'>
                 <button
