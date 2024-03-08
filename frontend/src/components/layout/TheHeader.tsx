49629cfb507e728b4392b6079a725eee2cd684f9
“antoanparashkevov” <tonkata1505@gmail.com>
2024-03-08 23:42:21 +0200

    Perform sign out operation when there is a user in the session

--- frontend/src/components/layout/TheHeader.tsx
+++ frontend/src/components/layout/TheHeader.tsx
@@ -1,25 +1,30 @@
-import React from "react";
+"use client";
+import React, {useEffect, useState} from "react";
 import Link from "next/link";
+import {getSession, signOut} from "@/lib/auth";
 
 import "./TheHeader.css";
-import {signOut} from "@/lib/auth";
 
 const TheHeader: React.FC = () => {
+	const [session, setSession] = useState<{ username: string; } | {}>({})
+
+	useEffect(() => {
+		getSession()
+			.then((response) => setSession(response))
+	}, []);
+
 	return (
 		<nav className="navbar">
 			<ul>
 				<li>
 					<Link
-						href="/"
-					>
-						Home
-					</Link>
-				</li>
-				<li>
-					<Link
 						href="/login"
+						onClick={async () => {
+							await signOut();
+							setSession({});
+						}}
 					>
-						Login
+						{session && session.hasOwnProperty("username") ? "Logout" : "Login"}
 					</Link>
 				</li>
 			</ul>
--- frontend_old/src/pages/Auth.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Auth: React.FC = () => {
-
-    return <h1>auth page</h1>
-
-}
-
-export default Auth;
