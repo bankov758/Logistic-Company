b850d5dcb6c969f2df12df9ce4198a3913f9c390
“antoanparashkevov” <tonkata1505@gmail.com>
2024-04-10 18:26:58 +0300

    Use different state for the loading indicator when we are loading the searched user

--- frontend/src/components/home/AdminInterface/AdminInterface.tsx
+++ frontend/src/components/home/AdminInterface/AdminInterface.tsx
@@ -42,6 +42,7 @@ const AdminInterface: React.FC = () => {
     const [selectedCompany, setSelectedCompany] = useState<selectorItem | null>(null);
 
     const [searchData, setSearchData ] = useState<item[] | null>(null);
+    const [isSearchingUser, setIsSearchingUser] = useState<boolean>(false);
     const [searchedUsername, setSearchedUsername] = useState("");
 
     const [showCompanyInfoDialog, setShowCompanyInfoDialog] = useState<boolean>(false)
@@ -79,16 +80,17 @@ const AdminInterface: React.FC = () => {
     }, [selectedCompany]);
 
     const fetchUserData = useCallback( () => {
-        setIsLoading(true);
+        setIsSearchingUser(true);
         axios.get(`/users?search=${searchedUsername}`)
             .then(response => {
                 setSearchData(response.data)
-                setIsLoading(false);
-                })
+                setIsSearchingUser(false);
+            })
             .catch(error => {
                 setError(error);
-                setIsLoading(false);});
-        }, [searchedUsername])
+                setIsSearchingUser(false);
+            });
+    }, [searchedUsername])
 
 
     useEffect(() => {
@@ -173,7 +175,7 @@ const AdminInterface: React.FC = () => {
                 <label htmlFor="clientName" className="" >Search for client with name: </label>
                 <input type="text" id="clientName" name="clientName" className="input-info-dialog" placeholder="username" value={searchedUsername}
                        onChange={(e) => setSearchedUsername(e.target.value)}/>
-                <Button fill={true} onClick={() => {fetchUserData(); setSelectedCompany(null); }}>Search</Button>
+                <Button fill={true} onClick={() => {fetchUserData(); setSelectedCompany(null); setData([]) }}>Search</Button>
         </div>
         {/* search for user by username END */}
 
@@ -292,7 +294,7 @@ const AdminInterface: React.FC = () => {
                 }) :
                 selectedCompany && <p>No data available!</p>
         }
-        {isLoading ?
+        {isSearchingUser ?
             <SkeletonLoadingAnimation header="tabs" layoutItems={5}/> :
             searchData && Object.keys(searchData).length > 0 && !selectedCompany ?
                 <Table
@@ -303,10 +305,10 @@ const AdminInterface: React.FC = () => {
                     data={searchData.map((item) => ({
                         ...item,
                         category: "clients"
-                    }))}/>
-                : searchedUsername && searchData && !searchData?.length && !selectedCompany ?
-                    <p>There is no found user with this username.</p>
-                : null
+                    }))}/> :
+            searchedUsername && searchData && !searchData?.length && !selectedCompany ?
+                    <p>There is no found user with this username.</p> :
+                    null
         }
     </>;
 }
